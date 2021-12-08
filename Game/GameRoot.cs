c81c00c55ed9a0ffd2170380ce216097ef1fb2fe
Jean-David Moisan <vynecompiler@gmail.com>
2021-12-08 09:58:46 -0500

    Initial MonoGame project

---
+++ Game/GameRoot.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Apos.Input;
+
+namespace GameProject {
+    public class GameRoot : Game {
+        public GameRoot() {
+            _graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _s = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            InputHelper.Setup(this);
+        }
+
+        protected override void Update(GameTime gameTime) {
+            InputHelper.UpdateSetup();
+
+            if (_quit.Pressed())
+                Exit();
+
+            // TODO: Add your update logic here
+
+            InputHelper.UpdateCleanup();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _s;
+
+        ICondition _quit =
+            new AnyCondition(
+                new KeyboardCondition(Keys.Escape),
+                new GamePadCondition(GamePadButton.Back, 0)
+            );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # MoltenDraw
 Infinite canvas drawing application.
+
+## Restore
+
+```
+dotnet restore Platforms/DesktopGL
+dotnet restore Platforms/WindowsDX
+```
+
+## Run
+
+```
+dotnet run --project Platforms/DesktopGL
+dotnet run --project Platforms/WindowsDX
+```
+
+## Debug
+
+In vscode, you can debug by pressing F5.
+
+## Publish
+
+```
+dotnet publish Platforms/DesktopGL -c Release -r win-x64 --output build-windows
+dotnet publish Platforms/DesktopGL -c Release -r osx-x64 --output build-osx
+dotnet publish Platforms/DesktopGL -c Release -r linux-x64 --output build-linux
+```
+
+```
+dotnet publish Platforms/WindowsDX -c Release -r win-x64 --output build-windowsdx
+```
